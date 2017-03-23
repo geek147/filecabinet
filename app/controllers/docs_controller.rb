9a13ed09513091ac9462c6935c7cd8800c7fa917
ilham malik <hebigami18@gmail.com>
2017-03-23 10:59:26 +0700

    new commit filecabinet

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -3,11 +3,10 @@ class DocsController < ApplicationController
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @docs = Doc.all.order("created_at DESC")
   end
 
   def show
-    @doc = Doc.find(params[:id])
   end
 
   def new
@@ -29,17 +28,22 @@ class DocsController < ApplicationController
   end
 
   def update
-
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @doc.destroy
+    redirect_to docs_path
   end
 
   private
 
     def find_doc
-
+      @doc = Doc.find(params[:id])
     end
 
     def doc_params
